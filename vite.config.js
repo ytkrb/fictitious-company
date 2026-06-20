64f2119ec965b4dc25257ab6f936109d3a2a8d35
ytkrb <e6WeK6eacc0Xj@gmail.com>
2026-06-20 15:21:18 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        en: resolve(__dirname, 'en/index.html'),
+      },
+    },
   },
 })
